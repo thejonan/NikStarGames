ec2b09a8156dba6398e7b50d20cdba0e59961a71
Nikola Georgiev <nikolaivanovgeorgiev@gmail.com>
2020-01-10 23:18:14 +0200

    New Grid

--- game.js
+++ game.js
@@ -1,14 +1,12 @@
-var MapArr = [[], [], []]
+var MapArr = [[], [], []],
 	turn = 0 ,
-	I,
-	i;
+	row,
+	col;
 function update() {
 
 }
 function draw() {
-	if (I = 0 || i = 0){
-		drawImage()
-	}
+	drawGrid(200);
 }
 function keydown(key) {
 	// if (key == 32){
@@ -16,24 +14,6 @@ function keydown(key) {
 	// }
 }
 function mouseup() {
-
-	if (mouseX >= 0 || mouseX < 200 ){
-		I = 0;
-	}
-	if (mouseX >= 200 || mouseX < 400){
-		I = 1;
-	}
-	if (mouseX >= 400 || mouseX < 600){
-		I = 2;
-	}
-
-	if (mouseY >= 0 || mouseY < 200){
-		i = 0;
-	}
-	if (mouseY >= 200 || mouseY < 400){
-		i = 1;
-	}
-	if (mouseY >= 400 || mouseY < 600){
-		i = 2;
-	}
+	row = Math.floor( mouseY / 200 );
+	col = Math.floor( mouseX / 200 );
 }
